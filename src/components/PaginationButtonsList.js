ecd2443812198e911c8ff6040b720b93da552d29
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-06-30 22:28:20 +0530

    commit

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 const PaginationButtonsList = () => {
+  let pages = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19.2
+  ];
+  return (
+    <div className="pagination-buttons-list">
+      {pages.map((ele, id) => {
+        return (
+          <button
+            className={ele === page ? "active-btn" : ""}
+            id={"button-${ele}"}
+            key={id}
+            onClick={() => {
+              clickHandler(ele);
+            }}
+          >
+            {ele}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
-    return (
-        <div className="pagination-buttons-list">
-
-        </div>
-    )
-}
-
-export { PaginationButtonsList }
+export { PaginationButtonsList };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,39 @@
-import React from 'react'
-
-
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../api/fetchPosts";
+import { PaginationButtonList } from "PaginationButtonList";
+import { post } from "./Post";
 const PostList = () => {
-    
-    return (
-        <>
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const loadData = async () => {
+    fetchPosts(page, 5)
+      .then((res) => res.json())
+      .then((dataj) => {
+        setData(dataj);
+      });
+  };
+  useEffect(() => loadData(), []);
+  useEffect(() => {
+    setData(null);
+    loadData();
+  }, [page]);
+  const clickHandler = (val) => {
+    setPage(val);
+  };
+  return (
+    <>
+      {data == null ? (
+        <div id="loader" className="loader">
+          loading
+        </div>
+      ) : (
+        data.map((ele) => {
+          return <Post ele={ele} key={ele.id} />;
+        })
+      )}
+      <PaginationButtonList page={page} clickHandler={clickHandler} />)
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export { PostList }
+export { PostList };
