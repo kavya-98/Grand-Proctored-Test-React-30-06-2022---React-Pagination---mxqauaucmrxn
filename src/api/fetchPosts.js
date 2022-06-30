eecc441f89d6605c72f68dd1f3849103e8b359ce
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-06-30 22:44:18 +0530

    comit

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,7 +1,6 @@
 const fetchPosts = async (page, limit) => {
-  let url =
-    "https://jsonplaceholder.typicode.com/posts?_page={page}&_limit={limit}";
-  fetch(url);
+  let url = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
+  return fetch(url);
 };
 
 export { fetchPosts };
